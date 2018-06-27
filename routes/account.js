46b284c46683481379625df0d1ba53aa6cd9ed1a
Phat Nguyen <phatnt1995@gmail.com>
2018-06-27 11:47:15 +0700

    Change password done

--- routes/account.js
+++ routes/account.js
@@ -1,5 +1,6 @@
 var express = require('express');
-
+var accountDao = require('../database/accountDAO');
+var md5 = require('md5');
 var router = express.Router();
 
 router.get('/', function (req, res, next) {
@@ -11,14 +12,39 @@ router.get('/', function (req, res, next) {
 
 router.get('/password', function (req, res, next) {
     res.render('account/password', {
-        title: 'Đổi mật khẩu | CamShop'
-    })
+        title: 'Đổi mật khẩu | CamShop',
+        succ: req.session.change_password_succ,
+        fail: req.session.change_password_fail
+    });
+
+    if(req.session.change_password_succ != undefined) {
+        delete req.session.change_password_succ;
+    }
+    
+    if(req.session.change_password_fail != undefined) {
+        delete req.session.change_password_fail;
+    }
+
 });
 
 router.get('/order', function(req, res, next) {
     res.render('account/order', {
         title: 'Quản lý đơn hàng | CamShop'
     })
-})
+});
+
+router.post('/password', function(req, res, next) {
+    if(req.body.old_password != req.session.user.matkhau) {
+        req.session.change_password_fail = true;
+        res.redirect('/account/password');
+    }
+    else {
+        var new_password = md5(req.body.new_password);
+        accountDao.changePassword(req.session.user.email, new_password).then(result => {
+            req.session.change_password_succ = true;
+            res.redirect('/account/password');
+        }); 
+    }
+});
 
 module.exports = router;
